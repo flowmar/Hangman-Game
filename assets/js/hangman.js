59c5124d3275b7b8665c3ce7db7d35cb91909cad
Flowmar <omar.imam@kineticvortex.com>
2017-08-05 17:37:44 -0500

    Add functionality to reset game and keep track of score, Finish commenting javascript code

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -26,6 +26,7 @@ var currentWord = "";
 var currentArray = [];
 var blanks = [];
 
+// jQuery DOM variables
 var winDisplay = $('#wins');
 var lossDisplay = $('#losses');
 var wordDisplay = $('#current-word');
@@ -47,37 +48,81 @@ var hangman = {
         remainingDisplay.html("Guesses Remaining: <br>" + remainingGuesses);
         guessedDisplay.html("Guessed Letters:<br>" + incorrectGuesses);
     },
+    // Creates an array of blanks dependent upon the current word
     createBlanks: function () {
         for (var i = 0; i < currentWord.length; i++) {
-            // If a hyphen is encountered, print 2 spaces
+            // If a hyphen is encountered, print a hyphen
             if (currentWord[i] == "-") {
                 blanks.push("-");
             } else {
-                // Otherwise, print a "_" character
+                // Otherwise, print a "*" character
                 blanks.push("*");
             }
         }
+        // Display the blanks array, joined together as a single string
         wordDisplay.html("Current Word <br>" + blanks.join(""));
     },
+    // Check to see if the currentGuess exists within the solution
     checkGuess: function (currentGuess) {
         if (currentArray.indexOf(currentGuess) >= 0) {
-            letterFound = true;
+            // If it does, run the replaceLetter function
             hangman.replaceLetter();
         } else {
+            // Otherwise, take away a remaining guess and push the incorrect guess to the
+            // incorrectGuesses array
             remainingGuesses -= 1;
             incorrectGuesses.push(currentGuess);
             console.log(incorrectGuesses);
+            // Refresh the stats on the page and check to see if the player has won or lost
             hangman.refreshStats();
+            hangman.checkWinLose();
         }
     },
     replaceLetter: function () {
+        // Loop through the currentWord...
         for (var i = 0; i < currentWord.length; i++) {
+            // Checking to see if the currentGuess exists in the solution
             if (currentGuess === currentWord.charAt(i)) {
+                // If it does, replace the element in the blanks array with the currentGuess
                 blanks[i] = currentGuess;
                 console.log(blanks);
+                // Update the 'Current Word' display
                 wordDisplay.html("Current Word: " + blanks.join(""));
+                // Check to see if the player has won or lost
+                hangman.checkWinLose();
             }
         }
+    },
+    checkWinLose: function () {
+        // If the player runs out of guesses, alert them, add 1 to 'losses', and
+        // refreshStats
+        if (remainingGuesses === 0) {
+            console.log("LOSE");
+            alert("YOU LOSE!");
+            losses += 1;
+            hangman.refreshStats();
+            hangman.resetGame(); // Otherwise, if the blanks array no longer contains any '*' characters, alert
+            // the player that they have won and add to their wins;
+        } else if (blanks.indexOf("*") < 0) {
+            alert("YOU WIN!!");
+            wins += 1;
+            // Refresh player stats
+            hangman.refreshStats();
+            hangman.resetGame();
+        }
+    },
+    // Resets the game and starts over
+    resetGame: function () {
+        currentWord = "";
+        currentArray = [];
+        currentGuess = "";
+        blanks = [];
+        incorrectGuesses = [];
+        remainingGuesses = 10;
+
+        hangman.chooseWord();
+        hangman.createBlanks();
+        hangman.refreshStats();
     }
 }
 
@@ -97,6 +142,7 @@ $(document).ready(function () {
                 // Remove the event listener for the 'Return' key
                 $(document).off("keyup");
 
+                // Hide the instructions when the game starts
                 $('.press-any').hide();
 
                 // Listen for the next letter that is pressed
